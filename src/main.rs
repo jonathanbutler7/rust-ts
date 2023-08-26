309d4e0c4b90258bb5b2a49f022b9341c72f15ba
Jonathan <jonathan.butler@getweave.com>
2023-08-26 14:39:34 -0500

    first exercises

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// fn main() {
+//     let foo: Vec<_> = vec![1, 2, 3]
+//         .iter()
+//         .map(|x| x + 1)
+//         .collect();
+
+//     println!("{:?}", foo)
+// }
+
+// below shows what collect does, but does it manually
 fn main() {
-    println!("Hello, world!");
+    let data = vec![1, 2, 3];
+    let mut foo = data.iter().map(|x| x + 1);
+    let mut new_vector = vec![];
+
+    while let Some(x) = foo.next() {
+        new_vector.push(x);
+    }
+
+    println!("{:?}", new_vector)
 }
