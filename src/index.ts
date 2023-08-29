62bbe5530ecd02172d7faf3a00e281e6c913a083
Jonathan <jonathan.butler@getweave.com>
2023-08-29 09:36:08 -0500

    add numbers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,67 @@
 const fs = require("fs");
 
-String(fs.readFileSync("lines"))
+// String(fs.readFileSync("lines"))
+//   .split("\n")
+//   .filter((_, index) => index % 2 === 0)
+//   .filter((_, index) => index > 1 && index < 4)
+//   .map((line) => console.log(line));
+
+// enum Color {
+//   Red,
+//   Green,
+//   Blue,
+//   Yellow,
+// }
+
+// function printColor(color: Color) {
+//   switch (color) {
+//     case Color.Red:
+//       console.log("red");
+//       break;
+//     case Color.Green:
+//       console.log("green");
+//       break;
+//     case Color.Blue:
+//       console.log("blue");
+//   }
+// }
+
+// printColor(Color.Red);
+
+// type Custom = {
+//   age: number;
+//   name: string;
+// };
+
+// type Item = number | string | Custom;
+
+// function append(items: Item[]) {
+//   items.push("Hello Fem");
+// }
+
+// const items: Item[] = [];
+// const numbers: number[] = [1, 2, 3];
+
+// append(items);
+// append(numbers);
+
+// console.log(numbers);
+
+// function thing(value: number | undefined): number | undefined {
+//   return value === undefined ? undefined : value * 5;
+// }
+
+// function practice(nums: number[], index: number) {
+//   return (nums[index] ?? index) * 5;
+// }
+
+const fileName = process.argv[2];
+
+fs.readFileSync(fileName)
+  .toString()
   .split("\n")
-  .filter((_, index) => index % 2 === 0)
-  .filter((_, index) => index > 1 && index < 4)
-  .map((line) => console.log(line));
+  .forEach((line: string) => {
+    const print = parseInt(line);
+    if (isNaN(print)) console.log("Line not a number");
+    console.log(line);
+  });
--- src/main.rs
+++ src/main.rs
@@ -39,19 +39,111 @@
 // ]);
 // }
 
-fn main() {
-    // let file = std::fs::read_to_string("lines").unwrap();
-    // file.lines()
-    //     .enumerate()
-    //     .filter(|(index, _)| index % 2 == 0)
-    //     .for_each(|(_, line)| println!("{}", line))
+// fn main() {
+//     // let file = std::fs::read_to_string("lines").unwrap();
+//     // file.lines()
+//     //     .enumerate()
+//     //     .filter(|(index, _)| index % 2 == 0)
+//     //     .for_each(|(_, line)| println!("{}", line))
 
-    let file = std::fs::read_to_string("lines").unwrap();
-    file.lines()
-        .enumerate()
-        .filter(|(index, _)| index % 2 == 0)
-        .skip(2)
-        .take(2)
-        .for_each(|(_, line)| println!("{}", line))
-        
+//     let file = std::fs::read_to_string("lines").unwrap();
+//     file.lines()
+//         .enumerate()
+//         .filter(|(index, _)| index % 2 == 0)
+//         .skip(2)
+//         .take(2)
+//         .for_each(|(_, line)| println!("{}", line))
+
+// }
+
+// enum Color {
+//     Red,
+//     Green,
+//     Blue,
+//     Yellow,
+// }
+
+// // fn is_green(color: Color) -> bool {
+// //     if color == Color::Green { true } else { false }
+// // }
+// impl Color {
+//     fn is_green(&self) -> bool {
+//         if let Color::Green = self {
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     fn is_green_parts(&self) -> bool {
+//         match self {
+//             Color::Red => {
+//                 return false;
+//             }
+//             Color::Green => {
+//                 return false;
+//             }
+//             Color::Blue => {
+//                 return true;
+//             }
+//             Color::Yellow => {
+//                 return true;
+//             }
+//         }
+//     }
+// }
+
+// fn print_color(color: Color) {
+//     match color {
+//         Color::Red => println!("red"),
+//         Color::Green => println!("green"),
+//         Color::Blue => println!("blue"),
+//         Color::Yellow => println!("yellow"),
+//     }
+// }
+
+// fn main() {
+//     let foo = Color::Green;
+//     foo.is_green();
+// }
+
+// struct Custom {
+//     age: usize,
+//     name: String,
+// }
+
+// enum Item {
+//     Number(usize),
+//     String(String),
+//     MyCustom(Custom),
+// }
+
+// fn append(items: &mut Vec<Item>) {
+//     // items.push(Item::String("Hello".to_string()))
+//     items.push(Item::String("Hello".into()))
+// }
+
+// fn main() {
+//     let mut items: Vec<Item> = vec![];
+//     append(&mut items);
+// }
+
+// fn multiply(num: Option<usize>) -> usize {
+//     return num.unwrap_or(0) * 5;
+// }
+
+fn main() {
+    let file_name = std::env::args().nth(1).expect("the file name to be passed in");
+    let file = std::fs::read_to_string(file_name).expect("unable to read file to string");
+    file.lines().for_each(|line| {
+        // let print:usize = line.parse();
+        if let Ok(value) = line.parse::<usize>() {
+            println!("{}", value)
+        } else {
+            println!("line not a number")
+        }
+    })
 }
+
+// fn multiply(nums: Vec<usize>, index: usize) -> usize {
+//     return nums.get(index).unwrap_or(&index) * 5;
+// }
